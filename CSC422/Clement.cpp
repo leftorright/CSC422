58983346603093f2fa9ee8d5debc2e915be73054
Stephanie Clement <sclement@uvic.ca>
2017-07-15 17:57:23 -0700

    add min dominating set algorithm

--- CSC422/Clement.cpp
+++ CSC422/Clement.cpp
@@ -21,7 +21,6 @@ struct graph_s {
     int dom[NMAX];
     int min_size;
     int min_dom[NMAX];
-    int verbose;
 };
 /* print_vector()
  *
@@ -29,9 +28,9 @@ struct graph_s {
  * input: size - number of elements in the array
  *        dom - an array
  */
-print_vector(int size, int *dom) {
+void print_vector(int size, int *dom) {
     for (int i = 0; i < size; i++) {
-        print("%d: %d", i, dom[i]);
+        printf("%d: %d", i, dom[i]);
     }
 }
 /* verbose=0    <graph num> <degree of vertices> <dominating set order>
@@ -89,11 +88,11 @@ int get_adj_list(graph_s *G)
             printf("Error: vertex %d has invalid degree=%d\n", i, i_degree);
             exit(0);
         }
-        G->max_degree = max_degree;
-        return 1;
     }
+    G->max_degree = max_degree;
+    return 1;
 }
-int initialize_BFS(graph_s *graph)
+void initialize_min_dom_set(graph_s *graph)
 {
     for (int i=0; i < graph->n; i++){
         graph->num_choice[i] = graph->degree[i] + 1;
@@ -103,10 +102,84 @@ int initialize_BFS(graph_s *graph)
     graph->n_dominated = 0;
     graph->size = 0;
     graph->min_size = graph->n;
-
 }
 
-void BFS(int level, graph_s *G)
+void array_copy(int *src, int *output, int size)
+{
+    for (int i = 0; i < size; i++) {
+        output[i] = src[i];
+    }
+}
+void dec_num_choice(graph_s *G, int u)
+{
+    G->num_choice[u]--;
+    for (int i = 0; i < G->degree[u]; i++){
+        int v = G->G[u][i];
+        G->num_choice[v]--;
+    }
+}
+void inc_num_choice(graph_s *G, int u)
+{
+    G->num_choice[u]++;
+    for (int i = 0; i < G->degree[u]; i++){
+        int v = G->G[u][i];
+        G->num_choice[v]++;
+    }
+}
+void inc_num_dominated(graph_s *G, int u)
+{
+    G->num_dominated[u]++;
+    for (int i=0; i < G->degree[u]; i++)
+    {
+        int v = G->G[u][i];
+        G->num_dominated[v]++;
+    }
+}
+void dec_num_dominated(graph_s *G, int u)
+{
+    G->num_dominated[u]--;
+    for (int i=0; i < G->degree[u]; i++)
+    {
+        int v = G->G[u][i];
+        G->num_dominated[v]--;
+    }
+}
+void update_n_dominated(graph_s *G, int u)
+{
+    for (int i=0; i < G->size; i++){
+        if (G->dom[i] == u){
+            return;
+        }
+    }
+    G->n_dominated++;
+}
+
+void update_dom(graph_s *G, int u)
+{
+    G->dom[G->size + 1] = u;
+    G->size++;
+}
+
+void restore_data_struct(graph_s *G, int u)
+{
+    // decrement num_dominated[v] - v neighbour of u and num_dominated[u]
+    dec_num_dominated(G, u);
+    // check if neighbours num_dominated go to 0 and then decrease those and itself
+    for (int i = 0; i < G->degree[u]; i++)
+    {
+        int v = G->G[u][i];
+        if (G->num_dominated[v] == 0){
+            G->n_dominated--;
+        }
+    }
+    if (G->num_dominated[u] == 0) {
+        G->n_dominated--;
+    }
+    // decrement size
+    G->size--;
+}
+
+void min_dom_set(int level, graph_s *G)
 {
 #if DEBUG
     printf("Level %3d: ", level);
@@ -118,21 +191,31 @@ void BFS(int level, graph_s *G)
     printf("Number of times dominated:\n");
     print_vector(G->n, G->num_dominated);
 #endif
-
-    for (int i = 0; i < G->n; i++) {
-        if (G->num_choice[i] == 0)
-            return;
+    if (G->n_dominated == G->n) {
+        if (G->size < G->min_size) {
+            array_copy(G->dom, G->min_dom, G->size);
+            G->min_size = G->size;
+            if (G->min_dom[0] != G->dom[0] || G->min_dom[1] != G->dom[1]) {
+                printf("WTF MATE!\n");
+            }
+        }
+        return;
     }
-    int u = level;
-    int n_extra = (G->n - G->n_dominated) / G->max_degree + 1; // XXX make this ceiling
-    if ( (G->size + n_extra) >= G->min_size)
+    int n_extra = (G->n - G->n_dominated + G->max_degree) / (G->max_degree + 1);
+    if ((G->size + n_extra) >= G->min_size)
         return;
 
-    G->num_choice[u]--;
-    for (int i = 0; i < G->degree[u]; i++){
-        int v = G->G[u][i];
-        G->num_choice[v]--;
-    }
+    //make blue
+    dec_num_choice(G, level);
+    min_dom_set(level + 1, G);
+    inc_num_choice(G, level);
+    //make red
+    inc_num_dominated(G, level);
+    update_n_dominated(G, level);
+    update_dom(G, level);
+    min_dom_set(level + 1, G);
+    // make white
+    restore_data_struct(G, level);
 }
 
 /* read_graph()
@@ -147,9 +230,9 @@ int read_graph(graph_s *graph)
     graph->graph_num = 0;
     int *n = &graph->n;
     while (get_next_int(n)) {
-        // check if i_degreeber of vertices in G is a valid i_degreeber
+        // check if number of vertices in G is a valid number
         if (*n <= 0 || *n > NMAX) {
-            printf("Error: Invalid number of vertices: %d. Should be a value between 1 and 2187\n", n);
+            printf("Error: Invalid number of vertices: %d. Should be a value between 1 and 2187\n", *n);
             return -1;
         }
         get_adj_list(graph);
@@ -161,6 +244,15 @@ int main(int argc, char *argv[])
 {
     graph_s graph;
     read_graph(&graph);
-    initialize_BFS(&graph);
-    BFS(0, &graph);
+    initialize_min_dom_set(&graph);
+    min_dom_set(0, &graph);
+
+    int verbose = atoi(argv[1]);
+    if (!(verbose == 0 || verbose == 1))
+    {
+        printf("Error: verbose flag must be set to 0 or 1.\n");
+        return 0;
+    }
+    printf("%5d %3d %3d\n", graph.graph_num, graph.n, graph.min_size);
+    fflush(stdout);
 }
